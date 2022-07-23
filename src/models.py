44df6acbb8033347727abe6ace40cb9644b2ab3f
melissanohemi <melissa.castro1118@gmail.com>
2022-07-23 15:20:46 +0000

    finished diagram

--- src/models.py
+++ src/models.py
@@ -8,26 +8,48 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    haircolor = Column(String)
+    eyecolor = Column(String)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    climated = Column(String)
+
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(30), unique=True)
+    password = Column(String(16))
+    email = Column(String, unique=True)
+    firstname = Column(String)
+    lastname = Column(String)
+    # name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Favorite(Base):
+    __tablename__ = 'favorite'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    character = relationship(Character)
+    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
+    planet = relationship(Planet)
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    user = relationship(User)
+    user_id = Column(Integer, ForeignKey('user.id'))
 
-    def to_dict(self):
-        return {}
+    def validate():
+        if not self.planet_id and not self.character_id:
+            return False
+        return True
+
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
